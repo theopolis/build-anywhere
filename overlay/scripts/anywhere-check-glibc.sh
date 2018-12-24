54c5eaf0f2cee0e80f5cfc60091b5e08ded43d7c
Teddy Reed <teddy@casualhacking.io>
2018-12-23 22:36:19 -0500

    Change anywhere-check-glibc to willitrun
    Example output from willitrun:
    
    $ willitrun $PREFIX/bin/icat
    ✓ $PREFIX/bin/icat: yes

--- overlay/scripts/anywhere-check-glibc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-[ "x$SYSROOT" = x""]; echo "Cannot find SYSROOT environment" && exit 1
-
-GLIBC_214=$(find $SYSROOT/usr/lib -type f | grep "\.so"  | xargs objdump -p  2>&1| grep GLIBC_2.14)
-
-[ ! "x$GLIBC_214" = x"" ]; echo "Found glibc 2.14 required" && exit 1
-
